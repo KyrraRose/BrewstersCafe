dfe7c6308bcf8d349c9bd77500f126c23b2d2859
KyrraRose <krbosland1@gmail.com>
2025-11-13 14:03:01 -0500

    Allowed generic receipt to calculate total, add display method later after building out other types of drinks

--- src/main/java/com/pluralsight/ui/Receipt.java
+++ src/main/java/com/pluralsight/ui/Receipt.java
@@ -1,4 +1,35 @@
 package com.pluralsight.ui;
 
-public class Receipt {
+import com.pluralsight.model.MenuItem;
+
+import java.util.ArrayList;
+
+public class Receipt<T extends MenuItem> {
+    private ArrayList<T> receipt;
+    private double orderTotal;
+
+    public Receipt() {
+        this.receipt = new ArrayList<>();
+        this.orderTotal = 0;
+    }
+
+    public void addToReceipt(T item){
+        receipt.add(item);
+    }
+
+    public ArrayList<T> getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(ArrayList<T> receipt) {
+        this.receipt = receipt;
+    }
+
+    public double getOrderTotal() {
+        for(T item : this.receipt){
+            this.orderTotal+= item.calculateTotal();
+        }
+        return this.orderTotal;
+    }
+
 }
